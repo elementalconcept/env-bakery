91a02badf9324ea4370a300cc62fc6f7c37d7e1a
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2021-06-16 12:18:36 +0100

    bakeEnv() now returns Angular environment

--- projects/lib/src/lib/bake-env.ts
+++ projects/lib/src/lib/bake-env.ts
@@ -1,8 +1,10 @@
-import { DotenvParseOutput, parse } from './dotenv-parse';
+import { parse } from './dotenv-parse';
 import { bakedEnv } from './baked-env';
 
-export async function bakeEnv<T extends DotenvParseOutput = DotenvParseOutput>(envUrl = '/assets/.env'): Promise<T> {
-  const result = parse<T>(await (await fetch(envUrl)).text());
+export async function bakeEnv<T>(envProvider: () => Promise<any>, envUrl = '/assets/.env'): Promise<T> {
+  const result = parse<Record<string, string>>(await (await fetch(envUrl)).text());
   Object.assign(bakedEnv, result);
-  return result;
+  const environment = (await envProvider()).environment;
+
+  return environment;
 }
