5e5d11ed6aa746bbf57d0c66c06f4c7fa0d52a62
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2021-06-16 10:17:43 +0100

    Library stub

---
+++ projects/lib/src/lib/lib.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LibComponent } from './lib.component';
+
+describe('LibComponent', () => {
+  let component: LibComponent;
+  let fixture: ComponentFixture<LibComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LibComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LibComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ projects/lib/src/lib/lib.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { LibComponent } from './lib.component';
+
+
+
+@NgModule({
+  declarations: [
+    LibComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    LibComponent
+  ]
+})
+export class LibModule { }
