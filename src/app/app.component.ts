eff4b3090998ad9d8716a7895bd00397b0f22f93
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2021-06-16 13:49:21 +0100

    Added usage example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { environment } from '../environments/environment';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
-  title = 'env-bakery';
+  readonly env = JSON.stringify(environment(), null, 2);
+
+  constructor() {
+  }
 }
