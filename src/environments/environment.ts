fefead626b2b5277019eaa8f0bbff2a2b9e24c61
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2021-06-16 13:48:34 +0100

    Added Firebase config to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,18 @@
 
 import { getEnv } from '@elemental-concept/env-bakery';
 
-export const environment = {
-  production: getEnv('PRODUCTION').boolean()
-};
+export const environment = () => ({
+  production: getEnv('PRODUCTION').boolean(),
+  firebase: {
+    apiKey: getEnv('FIREBASE_API_KEY').string(),
+    authDomain: getEnv('FIREBASE_AUTH_DOMAIN').string(),
+    projectId: getEnv('FIREBASE_PROJECT_ID').string(),
+    storageBucket: getEnv('FIREBASE_STORAGE_BUCKET').string(),
+    messagingSenderId: getEnv('FIREBASE_MESSAGING_SENDER_ID').string(),
+    appId: getEnv('FIREBASE_APP_ID').string(),
+    measurementId: getEnv('FIREBASE_MEASUREMENT_ID').string()
+  }
+});
 
 /*
  * For easier debugging in development mode, you can import the following file
