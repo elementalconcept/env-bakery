d61a22c5f9a7e0c88a7d151646a36a1f9ac0341c
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2021-06-16 13:49:09 +0100

    Angular environment is now a function

--- projects/lib/src/lib/bake-env.ts
+++ projects/lib/src/lib/bake-env.ts
@@ -4,7 +4,7 @@ import { bakedEnv } from './baked-env';
 export async function bakeEnv<T>(envProvider: () => Promise<any>, envUrl = '/assets/.env'): Promise<T> {
   const result = parse<Record<string, string>>(await (await fetch(envUrl)).text());
   Object.assign(bakedEnv, result);
-  const environment = (await envProvider()).environment;
+  const environment = (await envProvider()).environment();
 
   return environment;
 }
