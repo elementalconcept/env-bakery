8c7ed29a94bc70a887da8763e3e62905e6fcba04
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2021-06-16 12:19:58 +0100

    Switched app to using baked env on start up

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,10 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { getEnv } from '@elemental-concept/env-bakery';
+
 export const environment = {
-  production: false
+  production: getEnv('PRODUCTION').boolean()
 };
 
 /*
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { bakeEnv } from '@elemental-concept/env-bakery';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-bakeEnv().then(() => {
+bakeEnv(() => import('./environments/environment')).then((environment: any) => {
   if (environment.production) {
     enableProdMode();
   }
