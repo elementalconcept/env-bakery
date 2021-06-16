c5c8c9cc9c9abfb45ba162d0489d53ca04587583
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2021-06-16 11:34:02 +0100

    First implementation of getEnv() and EnvConverter

---
+++ projects/lib/src/lib/get-env.ts
@@ -0,0 +1,58 @@
+import { bakedEnv } from './baked-env';
+
+export function getEnv(key: string): EnvConverter {
+  return new EnvConverter(bakedEnv[ key ]);
+}
+
+export class EnvConverter {
+  constructor(private readonly value: any) {
+  }
+
+  raw = () => this.value;
+
+  number = (defaultValue = 0): number => {
+    switch (typeof this.value) {
+      case 'number':
+        return this.value;
+
+      case 'string':
+        const result = parseFloat(this.value);
+        return Number.isNaN(result) ? defaultValue : result;
+
+      default:
+        return defaultValue;
+    }
+  };
+
+  string = (defaultValue = ''): string => {
+    switch (typeof this.value) {
+      case 'number':
+        return this.value.toString(10);
+
+      case 'string':
+        return this.value;
+
+      default:
+        return defaultValue;
+    }
+  };
+
+  array = (separator = ',', defaultValue: string[] = []): string[] => {
+    if (typeof this.value === 'string') {
+      return this.value.split(separator);
+    }
+
+    return defaultValue;
+  };
+
+  boolean = (
+    truthyValues = [ 'true', 't', '1', 'on', 'enable', 'enabled', 'yes' ],
+    defaultValue = false): boolean => {
+
+    if (typeof this.value !== 'string') {
+      return defaultValue;
+    }
+
+    return truthyValues.includes(typeof this.value.toLowerCase());
+  };
+}
