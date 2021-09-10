0105cd60678d97c3af5b4c4f327fd6cc01406aa0
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2021-09-10 15:51:38 +0100

    Added final steps

--- projects/lib/schematics/ng-add/index.ts
+++ projects/lib/schematics/ng-add/index.ts
@@ -15,14 +15,18 @@ import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 export function ngAdd(options: any): Rule {
   return (tree: Tree, context: SchematicContext) => {
     context.addTask(new NodePackageInstallTask());
+
     return chain([
       mergeWith(apply(empty(), [boilerplate(options)])),
-      updateEnvironment()
+      updateEnvironment(),
+      updateMainTs(),
+      removeFileReplacements(),
+      finalize()
     ]);
   };
 }
 
-export function boilerplate(options: any): Rule {
+function boilerplate(options: any): Rule {
   return mergeWith(
     apply(
       url('./files'),
@@ -37,16 +41,16 @@ export function boilerplate(options: any): Rule {
   );
 }
 
-export function updateEnvironment(): Rule {
+function updateEnvironment(): Rule {
   const envStart = 'export const environment = {';
   const envEnd = '};';
   const prodFlag = 'production: false';
 
   const envStartReplace = 'export const environment = () => ({';
   const envEndReplace = '});';
-  const prodFlagReplace = 'production: getEnv(\'PRODUCTION\').boolean()';
+  const prodFlagReplace = `production: getEnv('PRODUCTION').boolean()`;
 
-  return (tree: Tree, context: SchematicContext) => {
+  return (tree: Tree) => {
     const path = 'src/environments/environment.ts';
     const file = tree.read(path);
     const source = file?.toString();
@@ -70,10 +74,110 @@ export function updateEnvironment(): Rule {
           .replace(prodFlag, prodFlagReplace);
 
       tree.overwrite(path, result);
+      console.log(`"${path}" was updated!`);
     } else {
       console.log(`File "${path}" does not have familiar code structure, skipping...`);
     }
 
     return tree;
   };
+}
+
+function updateMainTs(): Rule {
+  const envImport = `import { environment } from './environments/environment';`;
+  const mainStart = `if (environment.production) {`;
+  const mainEnd = `.catch(err => console.error(err));`;
+
+  const envImportReplace = `import { bakeEnv } from '@elemental-concept/env-bakery';`;
+  const mainStartReplace =
+    `bakeEnv(() => import('./environments/environment')).then((environment: any) => {\n\n${mainStart}`;
+  const mainEndReplace = `${mainEnd}\n\n});`;
+
+  return (tree: Tree) => {
+    const path = 'src/main.ts';
+    const file = tree.read(path);
+    const source = file?.toString();
+
+    if (source === undefined) {
+      console.log(`File "${path}" was not found, skipping...`);
+      return tree;
+    }
+
+    if (source.indexOf(envImport) >= 0 && source.indexOf(mainStart) >= 0 && source.indexOf(mainEnd) >= 0) {
+      const result = source
+        .replace(envImport, envImportReplace)
+        .replace(mainStart, mainStartReplace)
+        .replace(mainEnd, mainEndReplace);
+
+      tree.overwrite(path, result);
+      console.log(`"${path}" was updated!`);
+    } else {
+      console.log(`File "${path}" does not have familiar code structure, skipping...`);
+    }
+
+    return tree;
+  };
+}
+
+function removeFileReplacements(): Rule {
+  const fileReplacements = 'fileReplacements';
+  const envName = 'src/environments/environment.ts';
+
+  return (tree: Tree) => {
+    const path = 'angular.json';
+    const file = tree.read(path);
+    const source = file?.toString();
+
+    if (source === undefined) {
+      console.log(`File "${path}" was not found, skipping...`);
+      return tree;
+    }
+
+    const json = JSON.parse(source);
+
+    eachKey(
+      json,
+      fileReplacements,
+      (object, key) => {
+        const replacements = object[key];
+
+        if (replacements instanceof Array) {
+          object[key] = replacements.filter(item => item?.replace !== envName);
+        }
+      }
+    );
+
+    tree.overwrite(path, JSON.stringify(json, null, 2));
+    console.log(`"${path}" was updated!`);
+
+    return tree;
+  };
+}
+
+function finalize(): Rule {
+  return (tree: Tree) => {
+    console.log(`\n\n==========\n`);
+    console.log('@elemental-concept/env-bakery was installed!');
+    console.log('Please check logs above to see if any project files were successfully modified');
+    console.log('and check that all modifications were made correctly and your project is still working.');
+    console.log('\nYou can now delete environment files, except for src/environments/environment.ts.');
+    console.log(`\n==========\n\n`);
+
+    return tree;
+  };
+}
+
+function eachKey(object: any, key: string, callback: (source: any, key: string) => void): void {
+  Object.keys(object)
+    .forEach(k => {
+      if (k === key) {
+        callback(object, key);
+        return;
+      }
+
+      if (object[k] instanceof Object && !(object[k] instanceof Array)) {
+        eachKey(object[k], key, callback);
+        return;
+      }
+    });
 }
